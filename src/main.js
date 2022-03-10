6cb1e27ffc78a7aa26555eb77474c295e3f7c001
Mariannadtd <romanova.lenovo@gmail.com>
2022-03-10 21:57:39 +0300

    slider

--- src/main.js
+++ src/main.js
@@ -1,55 +1,61 @@
 //   Slider
-const prev = document.getElementById('btn-prev');
-	  next = document.getElementById('btn-next');
-	  slides = document.querySelectorAll('.slide');
-	  dots = document.querySelectorAll('.dot');	
+window.onload=function() {
+const prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
+      slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot'),
+      slidesWrap = document.querySelectorAll('.slider-wrapper');
 
 let index = 0;
 
 const activeSlide = n => {
-	for(slide of slides) {
-		slide.classList.remove('active');
-	}
-	slides[n].classList.add('active');
+    for(slide of slides){
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
 }
 
 const activeDot = n => {
-	for(dot of dots) {
-		dot.classList.remove('active');
-	}
-	dots[n].classList.add('active');
+    for(dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
 }
 
 const prepareCurrentSlide = ind => {
-	activeSlide(index);
-	activeDot(index);
+    activeSlide(ind);
+    activeDot(ind);
 }
 
 const nextSlide = () => {
-	if(index == slides.length - 1){
-		index = 0;
-		prepareCurrentSlide(index);
-	}else {
-		index++;
-		prepareCurrentSlide(index);
-	}
-}
+    if(index == slides.length - 1){
+        index = 0;
+        prepareCurrentSlide(index);
+    }else{
+        index++;
+        prepareCurrentSlide(index);
+    }
+};
 const prevSlide = () => {
-	if(index == 0){
-		index = slides.length - 1;
-		prepareCurrentSlide(index);
-	}else {
-		index--;
-		prepareCurrentSlide(index);
-	}
+    if(index == 0){
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    }else{
+        index--;
+        prepareCurrentSlide(index);
+    }
+};
+
+dots.forEach((item,indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+        clearInterval(interval);
+    })
+});
+
+
+
+const interval = setInterval(nextSlide, 2500);
+
 }
-
-dots.forEach((item, indexDot)=> {
-	item.addEventListener('click', () => {
-		index = indexDot;
-		prepareCurrentSlide(index);
-	})
-})
-
-
-
